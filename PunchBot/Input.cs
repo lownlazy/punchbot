80e72be1d0fba94fead0b46d829b057f16e6efc5
lownlazy <lownlazy@gmail.com>
2014-03-03 14:38:59 +0800

    add data binding sample code

---
+++ PunchBot/Input.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PunchBot
+{
+    public class Input
+    {
+        public ObservableCollection<KeyValuePair<string, int>> ValueList { get; private set; }
+
+        public Input()
+        {
+            this.ValueList = new ObservableCollection<KeyValuePair<string, int>>();
+            ValueList.Add(new KeyValuePair<string, int>("Developer", 60));
+            ValueList.Add(new KeyValuePair<string, int>("Misc", 20));
+            ValueList.Add(new KeyValuePair<string, int>("Tester", 50));
+            ValueList.Add(new KeyValuePair<string, int>("QA", 30));
+            ValueList.Add(new KeyValuePair<string, int>("Project Manager", 40));
+        }
+
+        public void Add(KeyValuePair<string, int> data)
+        {
+            ValueList.Add(data);
+        }
+    }
+}
--- PunchBot/MainWindow.xaml.cs
+++ PunchBot/MainWindow.xaml.cs
@@ -21,9 +21,29 @@ namespace PunchBot
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<KeyValuePair<string, int>> ValueList { get; private set; }
+        public PointCollection Points = new PointCollection(new Point[] { new Point(1, 1), new Point(2, 2) });
+        private Input data;
+
         public MainWindow()
         {
+            data = new Input();
             InitializeComponent();
+            this.DataContext = data;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //PointCollection points = new PointCollection(new Point[] { new Point(1, 1), new Point(2, 9) });
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.data.Add(new KeyValuePair<string, int>("XXX", 27));
+        }
+
+
+
     }
 }
